a09a7cdec702381fd34a20ef2df31d78e0f7048a
Michał Oprych <michal.oprych@student.pk.edu.pl>
2025-03-15 09:25:03 +0100

    init

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,43 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Next and Previous Button Test', function () {
+  it('Checks if next and previous button work', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active h1').should('contain', 'Rome');
+
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active h1').should('contain', 'London');
+
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active h1').should('contain', 'Rome');
+  });
+});
+
+describe('Descriptions Test', function () {
+  it('Checks if first description contains "Italy"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active p').should('contain', 'Italy');
+  });
+
+  it('Checks if second description contains "United Kingdom"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(1000);
+    
+    cy.get('.swiper-slide-active p').should('contain', 'United Kingdom');
+  });
+
+  it('Checks if third description contains "France"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(1000);
+    cy.get('.swiper-button-next').click();
+    cy.wait(1000);
+
+    cy.get('.swiper-slide-active p').should('contain', 'France');
+  });
+});
